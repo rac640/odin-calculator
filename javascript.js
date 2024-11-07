28d71d78bcd6e786e979a4e5a55cc4b427ad9628
rac640 <50179707+rac640@users.noreply.github.com>
2024-11-07 08:04:12 -0500

    Added Operator Buttons + Odin Instructions for organization

--- javascript.js
+++ javascript.js
@@ -1,3 +1,8 @@
+
+
+/* Your calculator is going to contain functions for all of the basic math operators you typically
+ find on calculators, so start by creating functions for the following items and testing them in your browser’s console:
+*/
 function additionFunction (a,b){
     isNaN(a) || isNaN(b) ?   console.log( "nope!"): console.log(a + b);
 }
@@ -15,4 +20,19 @@ function multiplyFunction (a,b){
 function divideFunction (a,b){
     isNaN(a) || isNaN(b) ?   console.log( "nope!"): console.log(a/b);
 
+}
+
+
+/* A calculator operation will consist of a number, an operator, and another number. For example, 3 + 5. 
+Create three variables, one for each part of the operation. You’ll use these variables to update your display later.
+*/
+let firstNumber;
+let operator;
+let secondNumber;
+
+
+// Create a new function operate that takes an operator and two numbers and then calls one of the above functions on the numbers.
+
+function operate (firstNumber,operator,secondNumber){
+additionFunction(firstNumber,secondNumber);
 }
