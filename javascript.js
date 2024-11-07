489c2d7799b36abafc865cf95d36b80619093805
rac640 <50179707+rac640@users.noreply.github.com>
2024-11-06 19:09:04 -0500

    Addition, subtraction, multiplication, division functions

---
+++ javascript.js
@@ -0,0 +1,18 @@
+function additionFunction (a,b){
+    isNaN(a) || isNaN(b) ?   console.log( "nope!"): console.log(a + b);
+}
+
+function subtractFunction (a,b){
+    isNaN(a) || isNaN(b) ?   console.log( "nope!"): console.log(a - b);
+
+}
+
+function multiplyFunction (a,b){
+    isNaN(a) || isNaN(b) ?   console.log( "nope!"): console.log(a * b);
+
+}
+
+function divideFunction (a,b){
+    isNaN(a) || isNaN(b) ?   console.log( "nope!"): console.log(a/b);
+
+}
