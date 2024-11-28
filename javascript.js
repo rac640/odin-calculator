18a83c5c53bfd60a9cd67576e0ef32350782939b
rac640 <50179707+rac640@users.noreply.github.com>
2024-11-27 20:00:15 -0500

    Pseudocode for entering zero as the first number

--- javascript.js
+++ javascript.js
@@ -1,19 +1,19 @@
 function additionFunction (a,b){
-   console.log(a + b);
+   return a + b;
 }
 
 function subtractFunction (a,b){
-    console.log(a - b);
+    return a - b;
 
 }
 
 function multiplyFunction (a,b){
-    console.log(a * b);
+    return a * b;
 
 }
 
 function divideFunction (a,b){
-  b == 0 ? alert("error: can't divide by 0"):console.log(a/b);
+  if (a == 0){ alert("error: can't divide by 0")} else{ return a/b} ;
 }
 
 let firstNumber;
@@ -46,72 +46,89 @@ displayBoard.appendChild(firstDisplayBoard);
 
 //  for each number button, when clicked, populate the display. 
 numberButton.forEach((button)=>{
+  // example: when clicking 1, create a div containing 1 on the display. 
   button.addEventListener("click", ()=>{
   const numberContainer = document.createElement("div");
   numberContainer.textContent = button.textContent;
   
-
   if (operatorClick == false ){
-
-  let displayedNumber = firstDisplayBoard.appendChild(numberContainer);
+  firstDisplayBoard.appendChild(numberContainer);
   let firstDisplayedNumber = firstDisplayBoard.textContent;
-firstNumber = firstDisplayedNumber;
-console.log("first Number:" + firstDisplayedNumber);
+  firstNumber = firstDisplayedNumber;
+
+
+  /*
+
+   I should reference firstDisplayBoard; if firstDisplayBoard[i] +1 or greater = 0, don't display.
+   Pseudocode: while the user clicks zero as the first number, keep clearing the display board and putting zero. 
+   
+   (while/if firstDisplayBoard.firstChild.textContent == 0, )
+
+  */
+  console.log(firstDisplayBoard.firstChild);
+  console.log("first Number:" + firstDisplayedNumber);
 
   }
-
+  // after the operator is clicked, clear the board and find a second number. 
 else{  
  
  if (secondNumber == undefined) {firstDisplayBoard.textContent = ""} ;
- // if (secondNumber !== undefined){
- //     displayBoard.textContent = '';
- //     secondNumber = secondNumber+  button.textContent; 
 
- // }
 
 secondDisplayBoard.appendChild(numberContainer);
 displayBoard.appendChild(secondDisplayBoard);
 
-//         let displayedNumber = firstDisplayBoard.appendChild(numberContainer);
- //  let firstDisplayedNumber = firstDisplayBoard.textContent;
-
 secondNumber = secondDisplayBoard.textContent;
 console.log("second Number:" + secondNumber);
 
-
 }
 
-})
+});
 
   });
 
 
-
-
-
 let additionButton = document.getElementById("additionButton");
 additionButton.addEventListener("click", ()=>{
-  
   console.log(firstNumber);
   operator = "+";
   operatorClick = true;
-
  }
-
-
-  
- 
 
 );
 
+let subtractionButton = document.getElementById("subtractionButton");
+subtractionButton.addEventListener("click", ()=>{
+  operator = "-";
+  operatorClick = true;
+});
 
+let multiplyButton = document.getElementById("multiplicationButton");
+multiplyButton.addEventListener("click",()=> {
+operator = "*";
+operatorClick = true;
+});
 
+let divisionButton = document.getElementById("divisionButton");
+divisionButton.addEventListener("click", ()=> {
+operator = "/";
+operatorClick = true;
 
-// 
+});
 
 let equalsButton = document.getElementById("equalsButton");
+equalsButton.addEventListener("click", ()=>{
 
+    
+  // Used Number: https://stackoverflow.com/questions/14496531/adding-two-numbers-concatenates-them-instead-of-calculating-the-sum 
+  let answer = operate(Number(firstNumber), operator, Number(secondNumber));
+  let answerBoard = document.createElement("div");
+  answerBoard.textContent = answer; 
 
+  if (answer !== undefined) {secondDisplayBoard.textContent = '';};
+
+displayBoard.appendChild(answerBoard);
+});
 
 
 
