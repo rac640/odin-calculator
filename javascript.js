c5aa4a649f5d7d6a1ee10214cac428df39d31410
rac640 <50179707+rac640@users.noreply.github.com>
2024-12-31 12:08:27 -0500

    Got evaluation of firstNumber and secondNumber to work

--- javascript.js
+++ javascript.js
@@ -1,8 +1,15 @@
-// this currentNumber variable  will serve as a "picture," capturing the current display content. 
+// this currentNumber variable  will serve as a "current picture," capturing the current display content. 
 let currentNumber ="";
+
+// the firstNumber and secondNumber are  "pictures" of the current number. 
 let firstNumber = "";
+
 // operator Variable will be used in finding the first and second numbers. 
 let operator = "";
+
+// The result will also be used when chaining operators 
+let result = "";
+
 
 
 // Step 1: Display number on board when button is clicked. 
@@ -51,3 +58,45 @@ function captureFirstNumber(){
     currentNumber = '';
     displayBoard.textContent = '';
 }
+
+
+// Step 4: Let user input currentNumber; on equalsButton click, Capture second number. Then, evaluate and provide a result. 
+
+const equalsButton = document.querySelector("#equalsButton");
+
+equalsButton.addEventListener("click",()=>{
+    captureSecondNumber();
+    captureResult();
+    displayBoard.textContent = result;
+
+});
+
+function captureSecondNumber(){
+    secondNumber = currentNumber; 
+    console.log("first Number: " + firstNumber);
+    console.log("second number: " + secondNumber)
+    currentNumber = '';
+    displayBoard.textContent = '';
+    
+}
+
+function captureResult(){
+
+    if (operator = "+"){
+        result = parseFloat(firstNumber) + parseFloat(secondNumber);
+        return result;
+    }
+    else if (operator = "-"){
+        result = parseFloat(firstNumber) - parseFloat(secondNumber);
+        return result;
+    }
+    else if (operator ="/"){
+        result = parseFloat(firstNumber) / parseFloat(secondNumber);
+        return result;
+    }
+    else if (operator = "*"){
+        result = parseFloat(firstNumber) * parseFloat(secondNumber);
+        return result;
+
+    }
+};
